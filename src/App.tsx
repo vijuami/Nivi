fd264233aef504d3289adf365cf160e9dc8f2fc4
vijuami <40740780+vijuami@users.noreply.github.com>
2025-09-03 12:29:38 +0530

    Updated package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './contexts/AuthContext';
-import { AuthGuard } from './components/AuthGuard';
 import { HomePage } from './components/HomePage';
 import { FinanceManagerApp } from './components/FinanceManagerApp';
 import { DocumentsPage } from './components/DocumentsPage';
@@ -9,26 +7,12 @@ import { VoiceDiaryPage } from './components/VoiceDiaryPage';
 
 function App() {
   return (
-    <AuthProvider>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/finance" element={
-          <AuthGuard>
-            <FinanceManagerApp />
-          </AuthGuard>
-        } />
-        <Route path="/documents" element={
-          <AuthGuard>
-            <DocumentsPage />
-          </AuthGuard>
-        } />
-        <Route path="/voice-diary" element={
-          <AuthGuard>
-            <VoiceDiaryPage />
-          </AuthGuard>
-        } />
-      </Routes>
-    </AuthProvider>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/finance" element={<FinanceManagerApp />} />
+      <Route path="/documents" element={<DocumentsPage />} />
+      <Route path="/voice-diary" element={<VoiceDiaryPage />} />
+    </Routes>
   );
 }
 
