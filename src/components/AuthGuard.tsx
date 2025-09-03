cc2dfebe5676ce70a87df89d9836976c22721d94
vijuami <40740780+vijuami@users.noreply.github.com>
2025-09-03 13:47:24 +0530

    Deleted jsconfig.json

--- src/components/AuthGuard.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-interface AuthGuardProps {
-  children: React.ReactNode
-}
-
-export const AuthGuard: React.FC<AuthGuardProps> = ({ children }) => {
-  return <>{children}</>
-}
